981d103d28a3d3091e05d47711d61fa48cc54324
Aldwin <122696394+dwinx00@users.noreply.github.com>
2024-03-24 17:27:05 +0800

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,29 @@
+
+class Menu:
+    def __init__(self):
+        self.__menu = {}
+
+    def get_choice(self, choices):
+        for x in range(len(choices)):
+            self.__menu[x + 1] = choices[x]
+
+    def show_menu(self):
+        for key, value in self.__menu.items():
+            print(f" [{key}] - {value}")
+
+    def get_input(self):
+        while True:
+            try:
+                input_choice = int(input("Enter the Choice: "))
+                if input_choice in self.__menu:
+                    return self.__menu[input_choice]  # Return the corresponding choice
+                else:
+                    print(f"Invalid Input. Please choose from 1 to {max(self.__menu)}")
+                    continue
+            except ValueError:
+                print("Invalid Input")
+
+
+
+
+
